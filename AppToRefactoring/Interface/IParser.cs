e8fc51c71eba2618ac817344c38e65e802a90a91
Medvedev Oleg <olegmedvmail@gmail.com>
2023-11-25 19:49:56 +0300

    add interface

---
+++ AppToRefactoring/Interface/IParser.cs
@@ -0,0 +1,16 @@
+﻿using AppToRefactoring.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppToRefactoring.Interface
+{
+    interface IParser
+    {
+        void Open(string path);
+        void Close();
+        List<Asset> ReadData();
+    }
+}
